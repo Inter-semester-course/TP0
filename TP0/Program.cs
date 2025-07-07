dbf1b5c6e740fe60264ca69e0f50f3fa7516173f
Fedora Anjara <hei.fedora.3@gmail.com>
2025-07-07 12:20:12 +0300

    finish the first TP

--- TP0/Program.cs
+++ TP0/Program.cs
@@ -29,19 +29,67 @@ namespace TP0
             Console.WriteLine("Entrez vos notes de SVT: ");
             SVT = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Entrez vos notes de Math: ");
-            MATH = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entrez vos notes de Malagasy: ");
+            MALG = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Entrez vos notes de Math: ");
-            MATH = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entrez vos notes de Francais: ");
+            FRS = Convert.ToInt32(Console.ReadLine());
 
 
 
             Console.WriteLine($"Bonjour {nomEtudiant} matricule {matricule}!");
 
+            Console.WriteLine(Moyenne(MATH, PC, SVT, MALG, FRS));
+            float moyenne=Moyenne(MATH, PC, SVT, MALG, FRS);
+
+            IsValidate(moyenne);
+
+            Mention(moyenne);
 
 
             Console.ReadKey();
         }
+
+        static float Moyenne(int MATH, int PC, int SVT, int MALG, int FRS)
+        {
+            return ((MATH + PC + SVT + MALG + FRS) / 5);
+        }
+
+        static void IsValidate(float Moyenne)
+        {
+            if (Moyenne >= 10)
+            {
+                Console.WriteLine("Felicitation vous avez validé");
+            }
+            else
+            {
+                Console.WriteLine("Malheureusement vous avez echoué");
+            }
+        }
+
+        static void Mention(float Moyenne)
+        {
+            if ((Moyenne >= 10) && (Moyenne <= 11.99))
+            {
+                Console.WriteLine("Mention Passable.");
+            }
+            else if ((Moyenne >= 12) && (Moyenne <= 13.99))
+            {
+                Console.WriteLine("Mention Assez bien.");
+            }
+            else if ((Moyenne >= 14) && (Moyenne <= 15.99))
+            {
+                Console.WriteLine("Mention Bien.");
+            }
+            else if ((Moyenne > 16))
+            {
+                Console.WriteLine("Mention Très Bien.");
+            }
+            else
+            {
+                Console.WriteLine("Echec. ");
+            }
+        }
     }
+
 }
